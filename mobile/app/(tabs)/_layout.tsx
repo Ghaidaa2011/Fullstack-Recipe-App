b0ee2285244edc176b6db99e1facbee2031aefa3
Ghaidaa <117023274+Ghaidaa2011@users.noreply.github.com>
2026-04-14 15:02:55 +0200

    add sign in screen

--- mobile/app/(tabs)/_layout.tsx
+++ mobile/app/(tabs)/_layout.tsx
@@ -2,6 +2,7 @@ import { useAuth } from "@clerk/expo";
 import { Redirect, Stack } from "expo-router";
 
 const TabsLayout = () => {
+  // Protected Routes
   const { isSignedIn } = useAuth();
   if (!isSignedIn) return <Redirect href={"./(auth)/sign-in"} />;
   return <Stack />;
--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,17 +1,24 @@
+import { COLORS } from "@/constants/colors";
 import { ClerkProvider } from "@clerk/expo";
 import { tokenCache } from "@clerk/expo/token-cache";
 import { Slot } from "expo-router";
+import { StatusBar } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
-
 if (!publishableKey) {
   throw new Error("Add your Clerk Publishable Key to the .env file");
 }
+
 const Provider = ClerkProvider as any;
+
 export default function RootLayout() {
   return (
     <Provider publishableKey={publishableKey} tokenCache={tokenCache}>
-      <Slot />
+      <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.background }}>
+        <StatusBar barStyle="dark-content" />
+        <Slot />
+      </SafeAreaView>
     </Provider>
   );
 }
