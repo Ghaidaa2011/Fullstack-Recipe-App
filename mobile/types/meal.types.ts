58a779c3b45393acaca0676ca6217628caeb2a37
Ghaidaa <117023274+Ghaidaa2011@users.noreply.github.com>
2026-04-15 22:07:40 +0200

    meal api

---
+++ mobile/types/meal.types.ts
@@ -0,0 +1,30 @@
+export interface MealAPIResponse {
+  idMeal: string;
+  strMeal: string;
+  strMealThumb: string;
+  strInstructions: string;
+  strCategory: string;
+  strArea: string;
+
+  // ingredients & measures (dynamic)
+  [key: string]: any;
+}
+export interface Meal {
+  id: string;
+  title: string;
+  description: string;
+  image: string;
+  cookTime: string;
+  servings: number;
+  category: string;
+  area: string;
+  ingredients: string[];
+  instructions: string[];
+  originalData: MealAPIResponse;
+}
+export interface Category {
+  idCategory: string;
+  strCategory: string;
+  strCategoryThumb: string;
+  strCategoryDescription: string;
+}
